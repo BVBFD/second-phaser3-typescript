15420a6b6263021a553f94c57a3b2ff31bd0072f
BVBFD <lsevina126@gmail.com>
2022-05-17 18:13:36 +0900

    code jetpack fire flames img

--- src/scenes/Preloader.ts
+++ src/scenes/Preloader.ts
@@ -1,6 +1,7 @@
 import Phaser from 'phaser'
 import TextureKeys from '~/consts/TextureKeys'
 import SceneKeys from '~/consts/SceneKeys'
+import AnimationKeys from '~/consts/AnimationKeys'
 
 export default class Preloader extends Phaser.Scene {
   constructor() {
@@ -12,6 +13,14 @@ export default class Preloader extends Phaser.Scene {
   preload() {
     this.load.image(TextureKeys.Background, 'house/bg_repeat_340x640.png')
 
+    this.load.image(TextureKeys.MouseHole, 'house/object_mousehole.png')
+
+    this.load.image(TextureKeys.Window1, 'house/object_window1.png')
+    this.load.image(TextureKeys.Window2, 'house/object_window2.png')
+
+    this.load.image(TextureKeys.Bookcase1, 'house/object_bookcase1.png')
+    this.load.image(TextureKeys.Bookcase2, 'house/object_bookcase2.png')
+
     this.load.atlas(
       TextureKeys.RocketMouse,
       'characters/rocket-mouse.png',
@@ -20,6 +29,31 @@ export default class Preloader extends Phaser.Scene {
   }
 
   create() {
+    this.anims.create({
+      key: AnimationKeys.RocketMouseRun,
+      frames: this.anims.generateFrameNames(TextureKeys.RocketMouse, {
+        start: 1,
+        end: 4,
+        prefix: 'rocketmouse_run',
+        zeroPad: 2,
+        suffix: '.png',
+      }),
+      frameRate: 10,
+      repeat: -1,
+    })
+
+    this.anims.create({
+      key: AnimationKeys.RocketMouseRun,
+      frames: this.anims.generateFrameNames(TextureKeys.RocketMouse, {
+        start: 1,
+        end: 2,
+        prefix: 'flame',
+        suffix: '.png',
+      }),
+      frameRate: 10,
+      repeat: -1,
+    })
+
     this.scene.start(SceneKeys.Game)
   }
 
